5e82f05c11c677017006817fa67300544da2efb0
Kitson Kelly <me@kitsonkelly.com>
2016-05-17 12:09:31 +0100

    Migrate to grunt-dojo2
    [ci skip]

---
+++ typings/browser.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="browser/ambient/dojo-compose/index.d.ts" />
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/jsdom/index.d.ts" />
+/// <reference path="browser/ambient/node/index.d.ts" />
+/// <reference path="browser/definitions/@reactivex/RxJS/index.d.ts" />
+/// <reference path="browser/definitions/immutable/index.d.ts" />
+/// <reference path="browser/definitions/maquette/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="main/ambient/dojo-compose/index.d.ts" />
+/// <reference path="main/ambient/es6-shim/index.d.ts" />
+/// <reference path="main/ambient/jsdom/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
+/// <reference path="main/definitions/@reactivex/RxJS/index.d.ts" />
+/// <reference path="main/definitions/immutable/index.d.ts" />
+/// <reference path="main/definitions/maquette/index.d.ts" />
