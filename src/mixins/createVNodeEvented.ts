dee40e6171c1ea16cdfbd35a0261805638a05b5b
Mark Wubben <mark@novemberborn.net>
2016-09-29 15:23:22 +0100

    Stop casting null to a VNodeListeners
    Casting null is not allowed when strict null checks is enabled. Instead
    provide a empty, frozen object.

--- src/mixins/createVNodeEvented.ts
+++ src/mixins/createVNodeEvented.ts
@@ -137,10 +137,12 @@ function handlesArraytoHandle(handles: Handle[]): Handle {
 	};
 }
 
+const UNINITIALIZED_LISTENERS = Object.freeze({});
+
 const createVNodeEvented: VNodeEventedFactory = createEvented
 	.mixin({
 		mixin: {
-			listeners: <VNodeListeners> null,
+			listeners: UNINITIALIZED_LISTENERS,
 
 			nodeAttributes: [
 				function (this: VNodeEvented): VNodeProperties {
@@ -159,7 +161,7 @@ const createVNodeEvented: VNodeEventedFactory = createEvented
 							* determine if the value is unitialized here, ensuring that this.listeners is
 							* always valid.
 							*/
-							if (this.listeners === null) {
+							if (this.listeners === UNINITIALIZED_LISTENERS) {
 								this.listeners = {};
 							}
 							let type: string;
