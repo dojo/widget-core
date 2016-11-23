9f4e3e27c20c947d2779251cc6bd09d9fdec44e7
Anthony Gubler <agubler@sitepen.com>
2016-11-23 13:39:02 +0000

    add unit tests for array equals

--- tests/unit/util/lang.ts
+++ tests/unit/util/lang.ts
@@ -1,6 +1,6 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
-import { insertInArray, stringToValue, valueToString, isChild } from '../../../src/util/lang';
+import { arrayEquals, insertInArray, stringToValue, valueToString, isChild } from '../../../src/util/lang';
 import createWidgetBase from '../../../src/bases/createWidgetBase';
 
 registerSuite({
@@ -94,6 +94,63 @@ registerSuite({
 				}, Error);
 			}
 		}
+	},
+	'arrayEquals': {
+		'array with reference are considered equal'() {
+			const array = [ 'a' ];
+
+			assert.isTrue(arrayEquals(array, array));
+		},
+		'falsy array references are not considered equal'() {
+			const array = [ 'a' ];
+
+			assert.isFalse(arrayEquals(array, <any> null));
+			assert.isFalse(arrayEquals(<any> null, array));
+			assert.isFalse(arrayEquals(<any> null, <any> null));
+		},
+		'simple array with matching entries are considered equal'() {
+			const array1 = [ 'a' ];
+			const array2 = [ 'a' ];
+
+			assert.isTrue(arrayEquals(array1, array2));
+			assert.isTrue(arrayEquals(array2, array1));
+		},
+		'multile entry array with mathing entries are considered equal'() {
+			const array1 = [ 'a', 'b' ];
+			const array2 = [ 'a', 'b' ];
+
+			assert.isTrue(arrayEquals(array1, array2));
+			assert.isTrue(arrayEquals(array2, array1));
+		},
+		'arrays with differing lengths are not considered equal'() {
+			const array1 = [ 'a' ];
+			const array2 = [ 'a', 'b' ];
+
+			assert.isFalse(arrayEquals(array1, array2));
+			assert.isFalse(arrayEquals(array2, array1));
+		},
+		'multile entry array with mathing entries in a different order are not considered equal'() {
+			const array1 = [ 'a', 'b' ];
+			const array2 = [ 'b', 'a' ];
+
+			assert.isFalse(arrayEquals(array1, array2));
+			assert.isFalse(arrayEquals(array2, array1));
+		},
+		'arrays with differing nested arrays are not considered equal'() {
+			const array1 = [ 'a', [ 'a', 'b' ] ];
+			const array2 = [ 'a', [ 'a' ] ];
+
+			assert.isFalse(arrayEquals(array1, array2));
+			assert.isFalse(arrayEquals(array2, array1));
+		},
+		'arrays with matching nested arrays are not considered equal'() {
+			const array1 = [ 'a', [ 'a', 'b' ] ];
+			const array2 = [ 'a', [ 'a', 'b' ] ];
+
+			assert.isTrue(arrayEquals(array1, array2));
+			assert.isTrue(arrayEquals(array2, array1));
+		}
+
 	},
 	'stringToValue()'() {
 		assert.isUndefined(stringToValue(''), 'emtpy string returns undefined');
