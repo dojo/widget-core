898bc436af34bf46df22395cee7fac1d8c040445
Anthony Gubler <agubler@sitepen.com>
2016-11-25 12:51:52 +0000

    update test name

--- tests/unit/all.ts
+++ tests/unit/all.ts
@@ -1,5 +1,6 @@
 import './createButton';
 import './projector'; // projector should load before integrations, because it loads a shim
+import './createProjector'; // projector should load before integrations, because it loads a shim
 import './integrations';
 import './main';
 import './mixins/all';
