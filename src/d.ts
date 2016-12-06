c5de707bc13dd111980c5815f39578230cd7d64a
Anthony Gubler <agubler@sitepen.com>
2016-12-06 11:46:26 +0000

    split 'd' into 'w' and 'v' and support getting factories from a registry.

--- src/d.ts
+++ src/d.ts
@@ -1,26 +1,26 @@
 import { ComposeFactory } from 'dojo-compose/compose';
 import {
+	Children,
 	DNode,
 	HNode,
 	WNode,
 	Widget,
-	WidgetState,
-	WidgetOptions
+	WidgetOptions,
+	WidgetState
 } from './interfaces';
-import { VNode, VNodeProperties } from 'dojo-interfaces/vdom';
-import { h } from 'maquette';
+import w from './w';
+import v from './v';
+import { VNodeProperties } from 'dojo-interfaces/vdom';
 
 export type TagNameOrFactory<S extends WidgetState, W extends Widget<S>, O extends WidgetOptions<S>> = string | ComposeFactory<W, O>;
 
 export type DOptions<S extends WidgetState, O extends WidgetOptions<S>> = VNodeProperties | O;
 
-export type Children = (DNode | VNode | null)[];
-
 function d(tagName: string, options: VNodeProperties, children?: Children): HNode;
 function d(tagName: string, children: Children): HNode;
 function d(tagName: string): HNode;
 function d<S extends WidgetState, W extends Widget<S>, O extends WidgetOptions<S>>(factory: ComposeFactory<W, O>, options: O): WNode;
-function d<S extends WidgetState, W extends Widget<S>, O extends WidgetOptions<S>>(factory: ComposeFactory<W, O>, options: O, children: DNode[]): WNode;
+function d<S extends WidgetState, W extends Widget<S>, O extends WidgetOptions<S>>(factory: ComposeFactory<W, O>, options: O, children: Children): WNode;
 function d<S extends WidgetState, W extends Widget<S>, O extends WidgetOptions<S>>(
 	tagNameOrFactory: TagNameOrFactory<S, W, O>,
 	optionsOrChildren: DOptions<S, O> = {},
@@ -28,27 +28,11 @@ function d<S extends WidgetState, W extends Widget<S>, O extends WidgetOptions<S
 ): DNode {
 
 	if (typeof tagNameOrFactory === 'string') {
-		if (Array.isArray(optionsOrChildren)) {
-			children = optionsOrChildren;
-			optionsOrChildren = {};
-		}
-
-		children = children.filter((child) => child);
-
-		return {
-			children: children,
-			render(this: { children: VNode[] }) {
-				return h(<string> tagNameOrFactory, <VNodeProperties> optionsOrChildren, this.children);
-			}
-		};
+		return v(tagNameOrFactory, <VNodeProperties> optionsOrChildren, children);
 	}
 
 	if (typeof tagNameOrFactory === 'function') {
-		return {
-			children: <DNode[]> children,
-			factory: tagNameOrFactory,
-			options: <WidgetOptions<WidgetState>> optionsOrChildren
-		};
+		return w(tagNameOrFactory, <WidgetOptions<WidgetState>> optionsOrChildren, children);
 	}
 
 	throw new Error('Unsupported tagName or factory type');
