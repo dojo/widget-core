a26f90a6d5262d3c23644254cb215e3a50a8f7f3
Anthony Gubler <agubler@sitepen.com>
2017-01-09 19:32:34 +0000

    external state mixin

---
+++ src/mixins/externalState.ts
@@ -0,0 +1,150 @@
+import { Handle } from 'dojo-interfaces/core';
+import { ObservablePatchableStore } from 'dojo-interfaces/abilities';
+import WeakMap from 'dojo-shim/WeakMap';
+import { assign } from 'dojo-core/lang';
+import { State, StatefulMixin } from 'dojo-interfaces/bases';
+import createEvented from 'dojo-compose/bases/createEvented';
+import { ComposeFactory } from 'dojo-compose/compose';
+
+/**
+ * Properties required for the external state mixin
+ */
+export interface ExternalStateProperties {
+	id: string;
+	externalState: ObservablePatchableStore<State>;
+}
+
+/**
+ * External State Options
+ */
+export interface ExternalStateOptions {
+	properties: ExternalStateProperties;
+}
+
+/**
+ * External State Mixin
+ */
+export interface ExternalStateMixin extends StatefulMixin<State> {
+	/**
+	 * Observe the state using the id and stateFrom in the instances properties
+	 */
+	observe(): void;
+}
+
+/**
+ * External State
+ */
+export interface ExternalState extends ExternalStateMixin {
+	properties: ExternalStateProperties;
+}
+
+/**
+ * Compose External State Factory interface
+ */
+export interface ExternalStateFactory extends ComposeFactory<ExternalStateMixin, ExternalStateOptions> {}
+
+/**
+ * internal state for the `ExternalStateMixin`
+ */
+interface InternalState {
+	id: string;
+	state: State;
+	handle: Handle;
+}
+
+/**
+ * Private map for external state.
+ */
+const internalStateMap = new WeakMap<ExternalStateMixin, InternalState>();
+
+/**
+ * state changed event type
+ */
+const stateChangedEventType = 'state:changed';
+
+function replaceState(instance: ExternalState, state: State) {
+	const internalState = internalStateMap.get(instance);
+	internalState.state = state;
+	const eventObject = {
+		type: stateChangedEventType,
+		state,
+		target: instance
+	};
+	instance.emit(eventObject);
+}
+
+/**
+ * ExternalState Factory
+ */
+const externalStateFactory: ExternalStateFactory = createEvented.mixin({
+	className: 'ExternalStateMixin',
+	mixin: {
+		get state(this: ExternalState) {
+			return internalStateMap.get(this).state;
+		},
+		observe(this: ExternalState): void {
+			const internalState = internalStateMap.get(this);
+			const { properties: { id, externalState } } = this;
+			if (!id || !externalState) {
+				throw new Error('id and stateFrom are required to observe state');
+			}
+
+			if (internalState) {
+				if (internalState.id === id) {
+					return;
+				}
+				throw new Error('Unable to observe state for a different id');
+			}
+
+			const subscription = externalState
+			.observe(id)
+			.subscribe(
+				(state) => {
+					replaceState(this, state);
+				},
+				(err) => {
+					throw err;
+				}
+			);
+
+			const handle = {
+				destroy: () => {
+					subscription.unsubscribe();
+					internalStateMap.delete(this);
+				}
+			};
+			internalStateMap.set(this, { id, handle, state: Object.create(null) });
+			this.own(handle);
+		},
+		setState(this: ExternalState, newState: Partial<State>): void {
+			const { properties: { externalState, id } } = this;
+			externalState.patch(assign( { id }, newState))
+			.then(() => externalState.get(id))
+			.then((state: State) => {
+				replaceState(this, state);
+			});
+		}
+	},
+	aspectAdvice: {
+		before: {
+			diffProperties(this: ExternalState, ...args: any[]): any[] {
+				const internalState = internalStateMap.get(this);
+				const { properties: { id, externalState } } = this;
+				const [ previousProperties ] = args;
+
+				if (internalState) {
+					if (externalState !== previousProperties.externalState || id !== previousProperties.id) {
+						internalState.handle.destroy();
+					}
+				}
+				return args;
+			},
+			applyChangedProperties(this: ExternalStateMixin, ...args: any[]) {
+				this.observe();
+				return args;
+			}
+		}
+	}
+});
+
+export default externalStateFactory;
---
+++ tests/unit/mixins/externalState.ts
@@ -0,0 +1,230 @@
+import compose from 'dojo-compose/compose';
+import * as registerSuite from 'intern!object';
+import * as assert from 'intern/chai!assert';
+import externalState from '../../../src/mixins/externalState';
+import { createObservableStore, ObservableStore } from 'dojo-stores/store/mixins/createObservableStoreMixin';
+
+let store: ObservableStore<{}, {}, any>;
+
+const externalStateWithProperties = compose({
+	properties: <any> {},
+	diffProperties(this: any, previousProperties: any) { },
+	applyChangedProperties(this: any) { }
+}).mixin(externalState);
+
+registerSuite({
+	name: 'mixins/externalStateMixin',
+	beforeEach() {
+		store = createObservableStore({
+			data: [
+				{ id: '1', foo: 'bar' },
+				{ id: '2', foo: 'bar' }
+			]
+		});
+	},
+	observe: {
+		'throw error if `id` property is not passed'() {
+			const externalStateMixin = externalStateWithProperties();
+
+			externalStateMixin.properties = {
+				externalState: store
+			};
+			assert.throws(() => externalStateMixin.observe(), Error);
+		},
+		'throw error if `externalState` property is not passed'() {
+			const externalStateMixin = externalStateWithProperties();
+
+			externalStateMixin.properties = {
+				id: '1'
+			};
+			assert.throws(() => externalStateMixin.observe(), Error);
+		},
+		observe() {
+			const externalStateMixin = externalStateWithProperties();
+
+			externalStateMixin.properties = {
+				id: '1',
+				externalState: store
+			};
+
+			const promise = new Promise((resolve, reject) => {
+				externalStateMixin.on('state:changed', ({ state }: any) => {
+					try {
+						assert.deepEqual(state, { id: '1', foo: 'bar'});
+						resolve();
+					} catch (err) {
+						reject(err);
+					}
+				});
+			});
+
+			externalStateMixin.observe();
+			externalStateMixin.observe();
+			return promise;
+		},
+		'throws error if observe called with a different id'() {
+			const externalStateMixin = externalStateWithProperties();
+
+			externalStateMixin.properties = {
+				id: '1',
+				externalState: store
+			};
+
+			externalStateMixin.observe();
+			externalStateMixin.properties.id = '2';
+			assert.throws(() => externalStateMixin.observe(), Error);
+
+		}
+	},
+	getState() {
+		const externalStateMixin = externalStateWithProperties();
+
+		externalStateMixin.properties = {
+			id: '1',
+			externalState: store
+		};
+
+		const promise = new Promise((resolve, reject) => {
+			externalStateMixin.on('state:changed', () => {
+				try {
+					assert.deepEqual(externalStateMixin.state, { id: '1', foo: 'bar'});
+					resolve();
+				} catch (err) {
+					reject(err);
+				}
+			});
+		});
+
+		externalStateMixin.observe();
+		assert.deepEqual(externalStateMixin.state, Object.create(null));
+		return promise;
+	},
+	setState() {
+		const externalStateMixin = externalStateWithProperties();
+		let intialStateChange = true;
+
+		externalStateMixin.properties = {
+			id: '1',
+			externalState: store
+		};
+
+		const promise = new Promise((resolve, reject) => {
+			externalStateMixin.on('state:changed', () => {
+				try {
+					if (intialStateChange) {
+						intialStateChange = false;
+						assert.deepEqual(externalStateMixin.state, { id: '1', foo: 'bar'});
+					}
+					else {
+						assert.deepEqual(externalStateMixin.state, { id: '1', foo: 'baz', baz: 'qux' });
+						resolve();
+					}
+				} catch (err) {
+					reject(err);
+				}
+			});
+		});
+
+		externalStateMixin.observe();
+		assert.deepEqual(externalStateMixin.state, Object.create(null));
+		externalStateMixin.setState({ id: '1', foo: 'baz', baz: 'qux' });
+		return promise;
+	},
+	diffProperties: {
+		'call destroy on observe handle if externalState has been updated.'() {
+			const externalStateMixin = externalStateWithProperties();
+			const newStore = createObservableStore({ data: [ { id: '1', foo: 'bar' } ]});
+			let intialStateChange = true;
+			const initialProperties = {
+				id: '1',
+				externalState: store
+			};
+
+			externalStateMixin.properties = initialProperties;
+
+			const promise = new Promise((resolve, reject) => {
+				externalStateMixin.on('state:changed', ({ target }: any) => {
+					try {
+						if (intialStateChange) {
+							intialStateChange = false;
+							assert.equal(target.properties.externalState, store);
+							externalStateMixin.properties = {
+								externalState: newStore,
+								id: '1'
+							};
+							externalStateMixin.diffProperties(initialProperties);
+							externalStateMixin.observe();
+						}
+						else {
+							assert.equal(target.properties.externalState, newStore);
+							resolve();
+						}
+					} catch (err) {
+						reject(err);
+					}
+				});
+			});
+
+			externalStateMixin.observe();
+			return promise;
+		},
+		'call destroy on observe handle if id has been updated.'() {
+			const externalStateMixin = externalStateWithProperties();
+			let intialStateChange = true;
+			const initialProperties = {
+				id: '1',
+				externalState: store
+			};
+
+			externalStateMixin.properties = initialProperties;
+
+			const promise = new Promise((resolve, reject) => {
+				externalStateMixin.on('state:changed', ({ target }: any) => {
+					try {
+						if (intialStateChange) {
+							intialStateChange = false;
+							assert.equal(target.properties.id, '1');
+							externalStateMixin.properties = {
+								externalState: store,
+								id: '2'
+							};
+							externalStateMixin.diffProperties(initialProperties);
+							externalStateMixin.observe();
+						}
+						else {
+							assert.equal(target.properties.id, '2');
+							resolve();
+						}
+					} catch (err) {
+						reject(err);
+					}
+				});
+			});
+
+			externalStateMixin.observe();
+			return promise;
+		}
+	},
+	applyChangedPropertie() {
+		const externalStateMixin = externalStateWithProperties();
+
+		externalStateMixin.properties = {
+			id: '1',
+			externalState: store
+		};
+
+		const promise = new Promise((resolve, reject) => {
+			externalStateMixin.on('state:changed', () => {
+				try {
+					assert.deepEqual(externalStateMixin.state, { id: '1', foo: 'bar' });
+					resolve();
+				} catch (err) {
+					reject(err);
+				}
+			});
+		});
+
+		externalStateMixin.applyChangedProperties();
+		return promise;
+	}
+});
