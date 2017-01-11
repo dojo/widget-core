ddb2a875f86a8dacceeee16f6c3324a2055bc2a9
Anthony Gubler <agubler@sitepen.com>
2017-01-11 01:10:43 +0000

    standardise on camel case for instance event handlers

--- src/components/textinput/createTextInput.ts
+++ src/components/textinput/createTextInput.ts
@@ -14,7 +14,7 @@ export type TextInputState = WidgetState & FormFieldMixinState<string>
 export type TextInputOptions = WidgetOptions<TextInputState, WidgetProperties> & FormFieldMixinOptions<string, TextInputState>;
 
 export type TextInput = Widget<TextInputState, WidgetProperties> & FormFieldMixin<string, TextInputState> & {
-	oninput(event: TypedTargetEvent<HTMLInputElement>): void;
+	onInput(event: TypedTargetEvent<HTMLInputElement>): void;
 };
 
 export interface TextInputFactory extends ComposeFactory<TextInput, TextInputOptions> { }
@@ -25,12 +25,12 @@ const createTextInput: TextInputFactory = createWidgetBase
 		mixin: {
 			type: 'text',
 			tagName: 'input',
-			oninput(this: TextInput, event: TypedTargetEvent<HTMLInputElement>) {
+			onInput(this: TextInput, event: TypedTargetEvent<HTMLInputElement>) {
 				this.value = event.target.value;
 			},
 			nodeAttributes: [
 				function(this: TextInput): VNodeProperties {
-					return { oninput: this.oninput };
+					return { oninput: this.onInput };
 				}
 			]
 		}
