138dee77332e2943b3be2c49a6123cb81080eca4
Tom Dye <github@tomdye.co.uk>
2017-01-12 16:56:51 +0000

    added cache

--- src/theme/themeManager.ts
+++ src/theme/themeManager.ts
@@ -19,7 +19,13 @@ export interface ThemeManager {
 
 export interface ThemeManagerFactory extends ComposeFactory<ThemeManager, {}> { }
 
-const themeManagerThemeMap = new WeakMap<ThemeManager, Theme>();
+type CacheKey = {
+	baseThemeClasses: {};
+	overrideClasses?: {};
+};
+
+const themeMap = new WeakMap<ThemeManager, Theme>();
+const themeManagerCacheMap = new WeakMap<ThemeManager, Map<CacheKey, ActiveClassMap<any>>>();
 
 function addClassNameToMap(classMap: ActiveClasses, classList: Theme, className: string) {
 	if (classList && classList.hasOwnProperty(className)) {
@@ -33,25 +39,33 @@ function addClassNameToMap(classMap: ActiveClasses, classList: Theme, className:
 
 const createThemeManager: ThemeManagerFactory = compose({
 	setTheme(this: ThemeManager, theme: {}) {
-		themeManagerThemeMap.set(this, theme);
+		themeMap.set(this, theme);
+		themeManagerCacheMap.set(this, new Map<CacheKey, ActiveClassMap<any>>());
 	},
 
 	getThemeClasses<T extends {}>(this: ThemeManager, baseThemeClasses: T, overrideClasses?: {}): ActiveClassMap<T> {
-		const loadedTheme = themeManagerThemeMap.get(this);
+		const cacheMap = themeManagerCacheMap.get(this);
+		const cacheKey = { baseThemeClasses, overrideClasses };
+		if (!cacheMap.has(cacheKey)) {
+			const loadedTheme = themeMap.get(this);
+			const activeClassMap = Object.keys(baseThemeClasses).reduce((activeClassMap, className) => {
+				const classMap: ActiveClasses = activeClassMap[<keyof T> className] = {};
+				let themeClassSource: Theme = baseThemeClasses;
 
-		return Object.keys(baseThemeClasses).reduce((activeClassMap, className) => {
-			const classMap: ActiveClasses = activeClassMap[<keyof T> className] = {};
-			let themeClassSource: Theme = baseThemeClasses;
+				if (loadedTheme && loadedTheme.hasOwnProperty(className)) {
+					themeClassSource = loadedTheme;
+				}
 
-			if (loadedTheme && loadedTheme.hasOwnProperty(className)) {
-				themeClassSource = loadedTheme;
-			}
+				addClassNameToMap(classMap, themeClassSource, className);
+				overrideClasses && addClassNameToMap(classMap, overrideClasses, className);
 
-			addClassNameToMap(classMap, themeClassSource, className);
-			overrideClasses && addClassNameToMap(classMap, overrideClasses, className);
+				return activeClassMap;
+			}, <ActiveClassMap<T>> {});
 
-			return activeClassMap;
-		}, <ActiveClassMap<T>> {});
+			cacheMap.set(cacheKey, activeClassMap);
+		}
+
+		return cacheMap.get(cacheKey);
 	}
 });
 
