739762b4adc22efc17e6be8fe7252077c259effd
Anthony Gubler <agubler@sitepen.com>
2017-01-13 18:54:03 +0000

    relative path changed after move to scoped package

--- tests/support/nls/greetings.ts
+++ tests/support/nls/greetings.ts
@@ -3,7 +3,7 @@ import has from '@dojo/has/has';
 // The default loader attempts to use the native Node.js `require` when running on Node. However, the Intern
 // suite uses the @dojo/loader, in which case the context for requires is the location of the loader module; or in
 // this case, `node_modules/@dojo/loader/loader.min.js'.
-const basePath = has('host-node') ? '../_build/' : '';
+const basePath = has('host-node') ? '../../_build/' : '';
 const bundlePath = `${basePath}tests/support/nls/greetings`;
 const locales = [ 'fr' ];
 
