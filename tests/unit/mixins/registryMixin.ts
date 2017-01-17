c68e3882f50b8e631f0ccca8621b8f38311ac90e
Matt Gadd <mgadd@sitepen.com>
2017-01-17 15:44:42 +0000

    add test for property change

--- tests/unit/mixins/registryMixin.ts
+++ tests/unit/mixins/registryMixin.ts
@@ -22,6 +22,21 @@ registerSuite({
 				properties: { registry }
 			});
 			assert.equal(instance.registry, registry);
+		},
+		'passed registry updated on property change'() {
+			const registry = new FactoryRegistry();
+			const newRegistry = new FactoryRegistry();
+			const instance = createRegistryWithProperties({
+				properties: { registry }
+			});
+			assert.equal(instance.registry, registry);
+			instance.emit({
+				type: 'properties:changed',
+				target: instance,
+				properties: { registry: newRegistry },
+				changedPropertyKeys: [ 'registry' ]
+			});
+			assert.equal(instance.registry, newRegistry);
 		}
 	}
 });
