3845ba6ac2ab391571a0f910ab9623552b202620
Rory Mulligan <rmulligan@sitepen.com>
2017-02-07 07:59:08 -0500

    Adding helpful types for event handlers, issue #240

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -239,3 +239,45 @@ export interface WidgetFactory<W extends WidgetMixin<P>, P extends WidgetPropert
 export interface TypedTargetEvent<T extends EventTarget> extends Event {
 	target: T;
 }
+
+/*
+ These are the event handlers exposed by Maquette.
+ */
+
+export type EventHandlerResult = boolean | void;
+
+export interface EventHandler {
+	(event?: Event): EventHandlerResult;
+}
+
+export interface FocusEventHandler {
+	(event?: FocusEvent): EventHandlerResult;
+}
+
+export interface KeyboardEventHandler {
+	(event?: KeyboardEvent): EventHandlerResult;
+}
+
+export interface MouseEventHandler {
+	(event?: MouseEvent): EventHandlerResult;
+}
+
+export type BlurEventHandler = FocusEventHandler;
+export type ChangeEventHandler = EventHandler;
+export type ClickEventHandler = MouseEventHandler;
+export type DoubleClicEventHandler = MouseEventHandler;
+export type InputEventHandler = EventHandler;
+export type KeyDownEventHandler = KeyboardEventHandler;
+export type KeyPressEventHandler = KeyboardEventHandler;
+export type KeyUpEventHandler = KeyboardEventHandler;
+export type LoadEventHandler = EventHandler;
+export type MouseDownEventHandler = MouseEventHandler;
+export type MouseEnterEventHandler = MouseEventHandler;
+export type MouseLeaveEventHandler = MouseEventHandler;
+export type MouseMoveEventHandler = MouseEventHandler;
+export type MouseOutEventHandler = MouseEventHandler;
+export type MouseOverEventHandler = MouseEventHandler;
+export type MouseUpEventHandler = MouseEventHandler;
+export type MouseWheelEventHandler = (event?: MouseWheelEvent | WheelEvent) => EventHandlerResult;
+export type ScrollEventHandler = (event?: UIEvent) => EventHandlerResult;
+export type SubmitEventHandler = EventHandler;
