f159efad6a02cbd388d9bf86c7333289fdadc537
Anthony Gubler <agubler@sitepen.com>
2017-02-07 21:19:42 +0000

    remove example code and custom Gruntfile config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,3 @@
 module.exports = function (grunt) {
-	var staticExampleFiles = [ 'src/examples/**', '!src/examples/**/*.js' ];
-
-	require('grunt-dojo2').initConfig(grunt, {
-		copy: {
-			staticExampleFiles: {
-				expand: true,
-				cwd: '.',
-				src: staticExampleFiles,
-				dest: '<%= devDirectory %>'
-			},
-			devStyles: {
-				expand: true,
-				cwd: '.',
-				src: 'src/styles/widgets.css',
-				dest: '<%= devDirectory %>'
-			},
-			distStyles: {
-				expand: true,
-				cwd: 'src',
-				src: 'styles/widgets.css',
-				dest: '<%= distDirectory %>'
-			}
-		}
-	});
-
-	grunt.registerTask('dev', grunt.config.get('devTasks').concat([
-		'copy:staticExampleFiles',
-		'postcss:modules-dev',
-		'copy:devStyles'
-	]));
-
-	grunt.registerTask('dist', grunt.config.get('distTasks').concat([
-		'postcss:modules-dist',
-		'postcss:variables',
-		'copy:distStyles'
-	]));
+	require('grunt-dojo2').initConfig(grunt, {});
 };
