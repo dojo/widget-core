cc568786d951569e10ba1ceb96528644e1c7c3af
Rory Mulligan <rmulligan@sitepen.com>
2017-04-07 07:29:06 -0400

    Adding diff property for bind

--- src/WidgetBase.ts
+++ src/WidgetBase.ts
@@ -122,6 +122,7 @@ function isHNodeWithKey(node: DNode): node is HNode {
 /**
  * Main widget base for all widgets to extend
  */
+@diffProperty('bind', DiffType.REFERENCE)
 export class WidgetBase<P extends WidgetProperties> extends Evented implements WidgetBaseInterface<P> {
 
 	/**
