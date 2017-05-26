65771bd198473b711afedb47fe7cbb4729998aec
Rory Mulligan <rmulligan@sitepen.com>
2017-05-26 08:52:33 -0400

    Removing CallbackMixin and replacing with requestAnimationFrame mocking

--- tests/unit/meta/Dimensions.ts
+++ tests/unit/meta/Dimensions.ts
@@ -4,30 +4,32 @@ import { WidgetBase } from '../../../src/WidgetBase';
 import { v } from '../../../src/d';
 import { ProjectorMixin } from '../../../src/main';
 import Dimensions from '../../../src/meta/Dimensions';
-import CallbackMixin from '../../support/CallbackMixin';
+import { stub } from 'sinon';
+
+let rAF: any;
+
+function resolveRAF() {
+	for (let i = 0; i < rAF.callCount; i++) {
+		rAF.getCall(0).args[0]();
+	}
+	rAF.reset();
+}
 
 registerSuite({
 	name: 'meta - Dimensions',
 
+	beforeEach() {
+		rAF = stub(global, 'requestAnimationFrame');
+	},
+
+	afterEach() {
+		rAF.restore();
+	},
+
 	'dimensions are blank on first render, set on second'(this: any) {
 		const dimensions: any[] = [];
-		const dfd = this.async();
 
-		class TestWidget extends CallbackMixin(2, dfd.callback(() => {
-			assert.strictEqual(dimensions.length, 2);
-			assert.deepEqual(dimensions[0], {
-				bottom: 0,
-				height: 0,
-				left: 0,
-				right: 0,
-				scrollHeight: 0,
-				scrollLeft: 0,
-				scrollTop: 0,
-				scrollWidth: 0,
-				top: 0,
-				width: 0
-			});
-		}), ProjectorMixin(WidgetBase))<any> {
+		class TestWidget extends ProjectorMixin(WidgetBase)<any> {
 			render() {
 				dimensions.push(this.meta(Dimensions).get('root'));
 				return v('div', {
@@ -43,14 +45,26 @@ registerSuite({
 
 		const widget = new TestWidget();
 		widget.append(div);
+
+		resolveRAF();
+
+		assert.strictEqual(dimensions.length, 2);
+		assert.deepEqual(dimensions[0], {
+			bottom: 0,
+			height: 0,
+			left: 0,
+			right: 0,
+			scrollHeight: 0,
+			scrollLeft: 0,
+			scrollTop: 0,
+			scrollWidth: 0,
+			top: 0,
+			width: 0
+		});
 	},
 
 	'dimensions has returns false for keys that dont exist'(this: any) {
-		const dfd = this.async();
-
-		class TestWidget extends CallbackMixin(2, dfd.callback(() => {
-			assert.isFalse(widget.getDimensions());
-		}), ProjectorMixin(WidgetBase))<any> {
+		class TestWidget extends ProjectorMixin(WidgetBase)<any> {
 			render() {
 				this.meta(Dimensions);
 
@@ -71,14 +85,12 @@ registerSuite({
 
 		const widget = new TestWidget();
 		widget.append(div);
+		resolveRAF();
+		assert.isFalse(widget.getDimensions());
 	},
 
 	'dimensions has returns true for keys that exist'(this: any) {
-		const dfd = this.async();
-
-		class TestWidget extends CallbackMixin(2, dfd.callback(() => {
-			assert.isTrue(widget.getDimensions());
-		}), ProjectorMixin(WidgetBase))<any> {
+		class TestWidget extends ProjectorMixin(WidgetBase)<any> {
 			render() {
 				this.meta(Dimensions);
 
@@ -99,5 +111,8 @@ registerSuite({
 
 		const widget = new TestWidget();
 		widget.append(div);
+		resolveRAF();
+
+		assert.isTrue(widget.getDimensions());
 	}
 });
