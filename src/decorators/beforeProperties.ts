72743c8326251c9e88ff65faedec2003de50da60
Anthony Gubler <agubler@sitepen.com>
2017-09-06 18:00:32 +0100

    doc

--- src/decorators/beforeProperties.ts
+++ src/decorators/beforeProperties.ts
@@ -2,7 +2,8 @@ import { handleDecorator } from './../WidgetBase';
 import { BeforeProperties } from './../interfaces';
 
 /**
- *
+ * Decorator that adds the function passed of target method to be run
+ * in the `beforeProperties` lifecycle.
  */
 export function beforeProperties(method: BeforeProperties): (target: any) => void;
 export function beforeProperties(): (target: any, propertyKey: string) => void;
