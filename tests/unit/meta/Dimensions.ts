ed9bc3e12fa08d0f4034ee03480f84128b59e9ec
Tom Dye <github@tomdye.co.uk>
2017-09-08 14:34:46 +0100

    dimensions tests

--- tests/unit/meta/Dimensions.ts
+++ tests/unit/meta/Dimensions.ts
@@ -2,14 +2,39 @@ import global from '@dojo/shim/global';
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import { stub } from 'sinon';
+// import { assign } from '@dojo/core/lang';
 // import { v } from '../../../src/d';
 // import { ProjectorMixin } from '../../../src/main';
-import Dimensions, { defaultDimensions } from '../../../src/meta/Dimensions';
+import Dimensions from '../../../src/meta/Dimensions';
 // import { WidgetBase } from '../../../src/WidgetBase';
 // import { ThemeableMixin } from './../../../src/mixins/Themeable';
 import NodeHandler, { Type } from '../../../src/NodeHandler';
 
 let rAF: any;
+const defaultDimensions = {
+	offset: {
+		height: 0,
+		left: 0,
+		top: 0,
+		width: 0
+	},
+	position: {
+		bottom: 0,
+		left: 0,
+		right: 0,
+		top: 0
+	},
+	scroll: {
+		height: 0,
+		left: 0,
+		top: 0,
+		width: 0
+	},
+	size: {
+		height: 0,
+		width: 0
+	}
+};
 
 function resolveRAF() {
 	for (let i = 0; i < rAF.callCount; i++) {
@@ -44,8 +69,6 @@ registerSuite({
 	},
 	'Will return default dimensions if node not loaded'() {
 		const nodeHandler = new NodeHandler();
-		// const element = document.createElement('div');
-		// nodeHandler.add(element, { key: 'foo' });
 
 		const dimensions = new Dimensions({
 			invalidate: () => {},
@@ -56,124 +79,33 @@ registerSuite({
 	},
 	'Will return element dimensions if node is loaded'() {
 		const nodeHandler = new NodeHandler();
-		const element = document.createElement('div');
-		nodeHandler.add(element, { key: 'foo' });
+
+		const offset = { offsetHeight: 10, offsetLeft: 10, offsetTop: 10, offsetWidth: 10 };
+		const scroll = { scrollHeight: 10, scrollLeft: 10, scrollTop: 10, scrollWidth: 10 };
+		const position = { bottom: 10, left: 10, right: 10, top: 10 };
+		const size = { width: 10, height: 10 };
+
+		const element = {
+			...offset,
+			...scroll,
+			getBoundingClientRect: stub().returns({
+				...position,
+				...size
+			})
+		};
+
+		nodeHandler.add(element as any, { key: 'foo' });
 
 		const dimensions = new Dimensions({
 			invalidate: () => {},
 			nodeHandler
 		});
 
-		assert.deepEqual(dimensions.get('foo'), defaultDimensions);
+		assert.deepEqual(dimensions.get('foo'), {
+			offset: { height: 10, left: 10, top: 10, width: 10 },
+			scroll: { height: 10, left: 10, top: 10, width: 10 },
+			position,
+			size
+		});
 	}
-	// ,
-	// 'Dimensions will call invalidate when nodehandler projector event fires'() {
-	// 	const nodeHandler = new NodeHandler();
-	// 	const element = document.createElement('div');
-	// 	nodeHandler.add(element, { key: 'foo' });
-	// 	const dimensions = new Dimensions({
-	// 		invalidate: () => {},
-	// 		nodeHandler
-	// 	});
-	// }
-	// 	const dimensions: any[] = [];
-
-	// 	class TestWidget extends ProjectorMixin(ThemeableMixin(WidgetBase))<any> {
-	// 		render() {
-	// 			dimensions.push(this.meta(Dimensions).get('root'));
-	// 			return v('div', {
-	// 				innerHTML: 'hello world',
-	// 				key: 'root'
-	// 			});
-	// 		}
-	// 	}
-
-	// 	const div = document.createElement('div');
-
-	// 	document.body.appendChild(div);
-
-	// 	const widget = new TestWidget();
-	// 	widget.append(div);
-
-	// 	resolveRAF();
-
-	// 	assert.strictEqual(dimensions.length, 2);
-	// 	assert.deepEqual(dimensions[0], {
-	// 		offset: {
-	// 			height: 0,
-	// 			left: 0,
-	// 			top: 0,
-	// 			width: 0
-	// 		},
-	// 		position: {
-	// 			bottom: 0,
-	// 			left: 0,
-	// 			right: 0,
-	// 			top: 0
-	// 		},
-	// 		scroll: {
-	// 			height: 0,
-	// 			left: 0,
-	// 			top: 0,
-	// 			width: 0
-	// 		},
-	// 		size: {
-	// 			height: 0,
-	// 			width: 0
-	// 		}
-	// 	});
-	// },
-
-	// 'dimensions has returns false for keys that dont exist'(this: any) {
-	// 	class TestWidget extends ProjectorMixin(ThemeableMixin(WidgetBase))<any> {
-	// 		render() {
-	// 			this.meta(Dimensions);
-
-	// 			return v('div', {
-	// 				innerHTML: 'hello world',
-	// 				key: 'root'
-	// 			});
-	// 		}
-
-	// 		getDimensions() {
-	// 			return this.meta(Dimensions).has('test');
-	// 		}
-	// 	}
-
-	// 	const div = document.createElement('div');
-
-	// 	document.body.appendChild(div);
-
-	// 	const widget = new TestWidget();
-	// 	widget.append(div);
-	// 	resolveRAF();
-	// 	assert.isFalse(widget.getDimensions());
-	// },
-
-	// 'dimensions has returns true for keys that exist'(this: any) {
-	// 	class TestWidget extends ProjectorMixin(ThemeableMixin(WidgetBase))<any> {
-	// 		render() {
-	// 			this.meta(Dimensions);
-
-	// 			return v('div', {
-	// 				innerHTML: 'hello world',
-	// 				key: 'root'
-	// 			});
-	// 		}
-
-	// 		getDimensions() {
-	// 			return this.meta(Dimensions).has('root');
-	// 		}
-	// 	}
-
-	// 	const div = document.createElement('div');
-
-	// 	document.body.appendChild(div);
-
-	// 	const widget = new TestWidget();
-	// 	widget.append(div);
-	// 	resolveRAF();
-
-	// 	assert.isTrue(widget.getDimensions());
-	// }
 });
