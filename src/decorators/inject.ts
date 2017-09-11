f077004d4831980c42244e9b02e621c88ec6fbc4
Anthony Gubler <agubler@sitepen.com>
2017-09-11 09:56:43 +0100

    add jsdoc

--- src/decorators/inject.ts
+++ src/decorators/inject.ts
@@ -3,17 +3,43 @@ import { handleDecorator, WidgetBase } from './../WidgetBase';
 import { beforeProperties } from './beforeProperties';
 import { RegistryLabel } from './../interfaces';
 
+/**
+ * Map of instances against registered injectors.
+ */
 const registeredInjectorsMap: WeakMap<WidgetBase, any[]> = new WeakMap();
 
+/**
+ * Defines the contract requires for the get properties function
+ * used to map the injected properties.
+ */
 export interface GetProperties<T = any> {
 	(context: any, properties: T): T;
 }
 
+/**
+ * Defines the inject configuration required for use of the `inject` decorator
+ */
 export interface InjectConfig {
+
+	/**
+	 * The label of the registry injector
+	 */
 	name: RegistryLabel;
+
+	/**
+	 * Function that returns propertues to inject using the passed properties
+	 * and the injected context.
+	 */
 	getProperties: GetProperties;
 }
 
+/**
+ * Decorator retrieves a context from an available registry using the name and
+ * returns calls the `getProperties` function with the existing found context
+ * and passed properties to return the injected properties.
+ *
+ * @param InjectConfig the inject configuration
+ */
 export function inject({ name, getProperties }: InjectConfig) {
 	return handleDecorator((target, propertyKey) => {
 		beforeProperties(function(this: WidgetBase, properties: any) {
