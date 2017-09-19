b7356ee2bae36ac8bdffd034b24c86a6bd90bb8a
Anthony Gubler <agubler@sitepen.com>
2017-09-19 14:38:26 +0100

    util _get for both get and getHandler

--- src/RegistryHandler.ts
+++ src/RegistryHandler.ts
@@ -38,52 +38,33 @@ export class RegistryHandler extends Evented {
 	}
 
 	public get<T extends WidgetBaseInterface = WidgetBaseInterface>(label: RegistryLabel, globalPrecedence: boolean = false): Constructor<T> | null {
-		const registries = globalPrecedence ? [ this._baseRegistry, this._registry ] : [ this._registry, this._baseRegistry ];
-
-		for (let i = 0; i < registries.length; i++) {
-			const registry = registries[i];
-			if (!registry) {
-				continue;
-			}
-			const item = registry.get<T>(label);
-			const registeredLabels = this._registryWidgetLabelMap.get(registry) || [];
-			if (item) {
-				return item;
-			}
-			else if (registeredLabels.indexOf(label) === -1) {
-				const handle = registry.on(label, (event: RegistryEventObject) => {
-					if (event.action === 'loaded' && this.get(label) === event.item) {
-						this.emit({ type: 'invalidate' });
-					}
-				});
-				this.own(handle);
-				this._registryWidgetLabelMap.set(registry, [ ...registeredLabels, label]);
-			}
-		}
-		return null;
+		return this._get(label, globalPrecedence, 'get', this._registryWidgetLabelMap);
 	}
 
 	public getInjector(label: RegistryLabel, globalPrecedence: boolean = false): Injector | null {
-		const registries = globalPrecedence ? [ this._baseRegistry, this._registry ] : [ this._registry, this._baseRegistry ];
+		return this._get(label, globalPrecedence, 'getInjector', this._registryInjectorLabelMap);
+	}
 
+	private _get(label: RegistryLabel, globalPrecedence: boolean, getFunctionName: 'getInjector' | 'get', labelMap: Map<Registry, RegistryLabel[]>): any {
+		const registries = globalPrecedence ? [ this._baseRegistry, this._registry ] : [ this._registry, this._baseRegistry ];
 		for (let i = 0; i < registries.length; i++) {
-			const registry = registries[i];
+			const registry: any = registries[i];
 			if (!registry) {
 				continue;
 			}
-			const item = registry.getInjector(label);
-			const registeredLabels = this._registryInjectorLabelMap.get(registry) || [];
+			const item = registry[getFunctionName](label);
+			const registeredLabels = labelMap.get(registry) || [];
 			if (item) {
 				return item;
 			}
 			else if (registeredLabels.indexOf(label) === -1) {
 				const handle = registry.on(label, (event: RegistryEventObject) => {
-					if (event.action === 'loaded' && this.getInjector(label) === event.item) {
+					if (event.action === 'loaded' && (<any> this)[getFunctionName](label) === event.item) {
 						this.emit({ type: 'invalidate' });
 					}
 				});
 				this.own(handle);
-				this._registryInjectorLabelMap.set(registry, [ ...registeredLabels, label]);
+				labelMap.set(registry, [ ...registeredLabels, label]);
 			}
 		}
 		return null;
