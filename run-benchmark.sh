dcbe57a496c56587c2e9f5901058d4655984aaef
Umar Hansa <umar.hansa@gmail.com>
2017-11-13 11:16:18 +0000

    Run benchmark script

---
+++ run-benchmark.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Clean up existing benchmark results, and don't fail if the folder does not exist
+rm html-report/results/*.json || true
+
+./node_modules/.bin/serve -p 8080 --silent &
+
+SERVER_PID=$!
+
+node _build/tests/benchmark/runner/src/benchmarkRunner.js --count 1 --headless false --framework dojo2-v0.2.0-non-keyed
+
+# Move the benchmark results somewhere else for now
+mkdir -p html-report
+mv results html-report
+
+# Use node to console.log since it gives pretty printing
+node -e "console.dir(require('./html-report/results/dojo2-v0.2.0-non-keyed_01_run1k.json'), {colors: true})"
+
+function cleanup {
+    kill $SERVER_PID
+}
+
+trap cleanup EXIT
