29f20fe3a2504674bd80b9ba7f6244027f4ba73e
Umar Hansa <umar.hansa@gmail.com>
2017-11-15 13:31:06 +0000

    Run both benchmarks in CI

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,45 +1,48 @@
 #!/usr/bin/env bash
 
-# Clean up existing benchmark results, and don't fail if the folder does not exist
-rm html-report/results/*.json || true
-
 ./node_modules/.bin/serve -p 8080 &
 SERVER_PID=$!
 
 echo '--- Benchmark starting ---'
 
-node _build/tests/benchmark/runner/src/benchmarkRunner.js --count 1 --headless false --framework dojo2-v0.2.0-non-keyed
+rm -rf benchmark-results
 
-node _build/tests/benchmark/runner/src/benchmarkRunner.js --count 1 --headless false --framework vanillajs-non-keyed
+node _build/tests/benchmark/runner/src/benchmarkRunner.js --count 3 --headless true --framework vanillajs-non-keyed
 
-# Move the benchmark results somewhere else for now
-mkdir -p html-report
-rm -rf html-report/benchmark-results
-mv results html-report/benchmark-results
+node _build/tests/benchmark/runner/src/benchmarkRunner.js --count 3 --headless true --framework dojo2-v0.2.0-non-keyed
+
 
 files=(
-	"dojo2-v0.2.0-non-keyed_01_run1k.json"
-	"dojo2-v0.2.0-non-keyed_02_replace1k.json"
-	"dojo2-v0.2.0-non-keyed_03_update10th1k.json"
-	"dojo2-v0.2.0-non-keyed_04_select1k.json"
-	"dojo2-v0.2.0-non-keyed_05_swap1k.json"
-	"dojo2-v0.2.0-non-keyed_06_remove-one-1k.json"
-	"dojo2-v0.2.0-non-keyed_07_create10k.json"
-	"dojo2-v0.2.0-non-keyed_08_create1k-after10k.json"
-	"dojo2-v0.2.0-non-keyed_09_clear10k.json"
-	"dojo2-v0.2.0-non-keyed_21_ready-memory.json"
-	"dojo2-v0.2.0-non-keyed_22_run-memory.json"
-	"dojo2-v0.2.0-non-keyed_23_update5-memory.json"
-	"dojo2-v0.2.0-non-keyed_24_run5-memory.json"
-	"dojo2-v0.2.0-non-keyed_25_run-clear-memory.json"
-	"dojo2-v0.2.0-non-keyed_30_startup.json"
+	"_01_run1k.json"
+	"_02_replace1k.json"
+	"_03_update10th1k.json"
+	"_04_select1k.json"
+	"_05_swap1k.json"
+	"_06_remove-one-1k.json"
+	"_07_create10k.json"
+	"_08_create1k-after10k.json"
+	"_09_clear10k.json"
+	"_21_ready-memory.json"
+	"_22_run-memory.json"
+	"_23_update5-memory.json"
+	"_24_run5-memory.json"
+	"_25_run-clear-memory.json"
+	"_30_startup.json"
 )
 
 echo 'Benchmark results: \n'
 for file in "${files[@]}"
 do
-	# Pretty printed JSON
-	node -e "console.dir(require('./html-report/benchmark-results/$file'), {colors: true})"
+	./node_modules/.bin/jq '.' benchmark-results/dojo2-v0.2.0-non-keyed$file
+	./node_modules/.bin/jq '.' benchmark-results/vanillajs-non-keyed$file
+done
+
+for file in "${files[@]}"
+do
+	dojoMedian="$(./node_modules/.bin/jq '.median' benchmark-results/dojo2-v0.2.0-non-keyed$file)"
+	vanillaJSMedian="$(./node_modules/.bin/jq '.median' benchmark-results/vanillajs-non-keyed$file)"
+
+	# node tests/benchmark/runner/output.ts
 done
 
 function cleanup {
