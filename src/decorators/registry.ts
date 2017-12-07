b6fd4ed0e77933afd2e2f9af44e09e1f1dd667d8
Tom Dye <github@tomdye.co.uk>
2017-12-07 17:42:05 +0000

    allow confug

--- src/decorators/registry.ts
+++ src/decorators/registry.ts
@@ -1,12 +1,22 @@
 import { handleDecorator } from './handleDecorator';
 
+export interface RegistryConfig {
+	[ name: string ]: () => Promise<any>;
+}
+
 /**
  * Decorator that can be used to register a widget with the registry
  */
-export function registry(name: string, loader: any) {
+export function registry(nameOrConfig: string | RegistryConfig, loader?: () => Promise<any>) {
 	return handleDecorator((target, propertyKey) => {
 		target.addDecorator('constructor', function(this: any) {
-			this.registry.define(name, loader);
+			if (typeof nameOrConfig === 'string') {
+				this.registry.define(nameOrConfig, loader);
+			} else {
+				Object.keys(nameOrConfig).forEach(name => {
+					this.registry.define(name, nameOrConfig[name]);
+				});
+			}
 		});
 	});
 }
