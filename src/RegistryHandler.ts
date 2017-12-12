70bf870b535c7d3509d15acca2701c4afc0c965f
Anthony Gubler <agubler@sitepen.com>
2017-12-12 09:58:40 +0000

    clear the base registry on destroy

--- src/RegistryHandler.ts
+++ src/RegistryHandler.ts
@@ -13,16 +13,26 @@ export class RegistryHandler extends Evented<RegistryHandlerEventMap> {
 	private _registry = new Registry();
 	private _registryWidgetLabelMap: Map<Registry, RegistryLabel[]> = new Map();
 	private _registryInjectorLabelMap: Map<Registry, RegistryLabel[]> = new Map();
-	protected baseRegistry: Registry;
+	protected baseRegistry?: Registry;
 
 	constructor() {
 		super();
 		this.own(this._registry);
+		const destroy = () => {
+			if (this.baseRegistry) {
+				this._registryWidgetLabelMap.delete(this.baseRegistry);
+				this._registryInjectorLabelMap.delete(this.baseRegistry);
+				this.baseRegistry = undefined;
+			}
+		};
+		this.own({ destroy });
 	}
 
 	public set base(baseRegistry: Registry) {
-		this._registryWidgetLabelMap.delete(this.baseRegistry);
-		this._registryInjectorLabelMap.delete(this.baseRegistry);
+		if (this.baseRegistry) {
+			this._registryWidgetLabelMap.delete(this.baseRegistry);
+			this._registryInjectorLabelMap.delete(this.baseRegistry);
+		}
 		this.baseRegistry = baseRegistry;
 	}
 
@@ -35,11 +45,11 @@ export class RegistryHandler extends Evented<RegistryHandlerEventMap> {
 	}
 
 	public has(label: RegistryLabel): boolean {
-		return this._registry.has(label) || this.baseRegistry.has(label);
+		return this._registry.has(label) || Boolean(this.baseRegistry && this.baseRegistry.has(label));
 	}
 
 	public hasInjector(label: RegistryLabel): boolean {
-		return this._registry.hasInjector(label) || this.baseRegistry.hasInjector(label);
+		return this._registry.hasInjector(label) || Boolean(this.baseRegistry && this.baseRegistry.hasInjector(label));
 	}
 
 	public get<T extends WidgetBaseInterface = WidgetBaseInterface>(label: RegistryLabel, globalPrecedence: boolean = false): Constructor<T> | null {
